9a869e03235730064f7d30ba87b4df0e2aa6712a
nikolay <www_nick2590@mail.ru>
2024-03-25 18:41:22 +0300

    add CompanyService

--- app/Services/Company/CompanyService.php
+++ app/Services/Company/CompanyService.php
@@ -1,13 +1,24 @@
 <?php
 
 namespace App\Services\Company;
+
+use App\Services\Company\Dto\CompanyDto;
+use App\Services\Company\Dto\CreateCompanyDto;
+use Ramsey\Uuid\UuidInterface;
+
 interface CompanyService
 {
-    public function find();
+    /**
+     * @param  UuidInterface  $companyId
+     * @return CompanyDto
+     */
+    public function find(UuidInterface $companyId): CompanyDto;
+
+    public function findMany(array $companyIds);
 
     public function delete();
 
-    public function create ();
+    public function create(CreateCompanyDto $createCompanyDto);
 
     public function update();
 }
--- app/Services/Company/LaravelCompanyService.php
+++ app/Services/Company/LaravelCompanyService.php
@@ -1,21 +1,53 @@
 <?php
 
 namespace App\Services\Company;
+
+use App\Services\Company\Dto\CompanyDto;
+use App\Services\Company\Dto\CreateCompanyDto;
+use App\Services\Company\Events\CreateCompanyEvent;
+use App\Services\Company\Repositories\CompanyRepository;
+use Illuminate\Events\Dispatcher;
+use Ramsey\Uuid\UuidInterface;
+
 final readonly class LaravelCompanyService implements CompanyService
 {
-    public function find(){
-      return null;
+    public function __construct(
+        private Dispatcher $dispatcher,
+        private CompanyRepository $companyRepository
+    ) {
     }
 
-    public function delete(){
-      return null;
+    public function find(UuidInterface $companyId): CompanyDto
+    {
+        return $this->companyRepository->find($companyId);
     }
 
-    public function create (){
-      return null;
+    /**
+     * @param  array  $companyIds
+     * @return CompanyDto[]
+     */
+    public function findMany(array $companyIds): array
+    {
+        return $this->companyRepository->findMany($companyIds);
     }
 
-    public function update(){
-      return null;
+
+    public function create(CreateCompanyDto $createCompanyDto)
+    {
+        $company = $this->companyRepository->create($createCompanyDto);
+
+        $this->dispatcher->dispatch(new CreateCompanyEvent($company->getUuid()));
+
+        return $company;
+    }
+
+    public function update()
+    {
+        return null;
+    }
+
+    public function delete()
+    {
+        return null;
     }
 }
