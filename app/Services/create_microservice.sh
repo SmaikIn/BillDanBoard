861f3e6942dd99395fed570341b3f497b3d52567
backendns <www_nick2590@mail.ru>
2024-03-27 16:46:01 +0300

    add script

--- app/Services/create_microservice.sh
+++ app/Services/create_microservice.sh
@@ -10,31 +10,43 @@ cd $serviceName || exit
 # Создаем папки Dto и Repositories
 mkdir Dto
 mkdir Repositories
+mkdir Events
+mkdir Exceptions
 
 # Создаем файлы LaravelNameService.php и NameService.php
 cat <<EOF > Laravel${serviceName}Service.php
 <?php
 
 namespace App\Services\\${serviceName};
+
+use App\Services\\${serviceName}\Repositories\\${serviceName}Repository;
+use App\Services\\${serviceName}\Dto\Create${serviceName}Dto;
+use App\Services\\${serviceName}\Dto\Update${serviceName}Dto;
 final readonly class Laravel${serviceName}Service implements ${serviceName}Service
 {
     public function __construct(
+      private ${serviceName}Repository \$repository,
     ){
     }
-    public function find(){
-      return null;
+
+    public function find(int \$id){
+      return \$this->repository->find(\$id);
     }
 
-    public function delete(){
-      return null;
+    public function findMany(array \$arrayIds){
+      return \$this->repository->findMany(\$arrayIds);
     }
 
-    public function create (){
-      return null;
+    public function delete(int \$id){
+      return \$this->repository->delete(\$id);
     }
 
-    public function update(){
-      return null;
+    public function create(Create${serviceName}Dto \$dto){
+      return \$this->repository->create(\$dto);
+    }
+
+    public function update(Update${serviceName}Dto \$dto){
+       return \$this->repository->update(\$dto);
     }
 }
 EOF
@@ -43,15 +55,20 @@ cat <<EOF > ${serviceName}Service.php
 <?php
 
 namespace App\Services\\${serviceName};
+
+use App\Services\\${serviceName}\Dto\Create${serviceName}Dto;
+use App\Services\\${serviceName}\Dto\Update${serviceName}Dto;
 interface ${serviceName}Service
 {
-    public function find();
+    public function find(int \$id);
 
-    public function delete();
+    public function findMany(array \$arrayIds);
 
-    public function create ();
+    public function delete(int \$id);
 
-    public function update();
+    public function create(Create${serviceName}Dto \$dto);
+
+    public function update(Update${serviceName}Dto \$dto);
 }
 EOF
 
@@ -60,6 +77,7 @@ cat <<EOF > Dto/${serviceName}Dto.php
 <?php
 
 namespace App\Services\\${serviceName}\Dto;
+
 final readonly class ${serviceName}Dto
 {
     public function __construct(
@@ -68,13 +86,55 @@ final readonly class ${serviceName}Dto
 }
 EOF
 
+# Создаем файл NameDto.php в папке Dto
+cat <<EOF > Dto/Update${serviceName}Dto.php
+<?php
+
+namespace App\Services\\${serviceName}\Dto;
+
+final readonly class Update${serviceName}Dto
+{
+    public function __construct(
+    ){
+    }
+
+}
+EOF
+
+# Создаем файл NameDto.php в папке Dto
+cat <<EOF > Dto/Create${serviceName}Dto.php
+<?php
+
+namespace App\Services\\${serviceName}\Dto;
+
+final readonly class Create${serviceName}Dto
+{
+    public function __construct(
+    ){
+    }
+
+}
+EOF
+
 # Создаем файлы DatabaseNameRepository и CacheNameRepository в папке Repositories
 cat <<EOF > Repositories/${serviceName}Repository.php
 <?php
 
 namespace App\Services\\${serviceName}\Repositories;
+
+use App\Services\\${serviceName}\Dto\Create${serviceName}Dto;
+use App\Services\\${serviceName}\Dto\Update${serviceName}Dto;
 interface ${serviceName}Repository
 {
+    public function find(int \$id);
+
+    public function findMany(array \$arrayIds);
+
+    public function delete(int \$id);
+
+    public function create(Create${serviceName}Dto \$dto);
+
+    public function update(Update${serviceName}Dto \$dto);
 }
 EOF
 
@@ -82,10 +142,33 @@ cat <<EOF > Repositories/Database${serviceName}Repository.php
 <?php
 
 namespace App\Services\\${serviceName}\Repositories;
+
+use App\Services\\${serviceName}\Dto\Create${serviceName}Dto;
+use App\Services\\${serviceName}\Dto\Update${serviceName}Dto;
 final readonly class Database${serviceName}Repository implements ${serviceName}Repository
 {
     public function __construct(
     ){
+    }
+
+    public function find(int \$id){
+    //TODO logic
+    }
+
+    public function findMany(array \$arrayIds){
+    //TODO logic
+    }
+
+    public function delete(int \$id){
+    //TODO logic
+    }
+
+    public function create(Create${serviceName}Dto \$dto){
+    //TODO logic
+    }
+
+    public function update(Update${serviceName}Dto \$dto){
+    //TODO logic
     }
 }
 EOF
@@ -94,12 +177,40 @@ cat <<EOF > Repositories/Cache${serviceName}Repository.php
 <?php
 
 namespace App\Services\\${serviceName}\Repositories;
+
+use App\Services\\${serviceName}\Dto\Create${serviceName}Dto;
+use App\Services\\${serviceName}\Dto\Update${serviceName}Dto;
 final readonly class Cache${serviceName}Repository implements ${serviceName}Repository
 {
     public function __construct(
     ){
     }
+
+    public function find(int \$id){
+    //TODO logic
+    }
+
+    public function findMany(array \$arrayIds){
+    //TODO logic
+    }
+
+    public function delete(int \$id){
+    //TODO logic
+    }
+
+    public function create(Create${serviceName}Dto \$dto){
+    //TODO logic
+    }
+
+    public function update(Update${serviceName}Dto \$dto){
+    //TODO logic
+    }
+
+    private function formatToDto(Model ${serviceName}){
+    //TODO logic
+    }
 }
 EOF
 
 echo "Папка с микросервисом ${serviceName} успешно создана!"
+echo "Не забудьте определить реализацию интерфейсов в AppServiceProvider"
