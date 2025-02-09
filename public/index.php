7f4c6679b1e16ecac96c1173b0445476424a7d75
nikolay <www_nick2590@mail.ru>
2025-02-09 16:51:12 +0300

    Revert "add"
    This reverts commit 81820376e996be969e10a23b670a41664735f44e.

--- public/index.php
+++ public/index.php
@@ -48,9 +48,8 @@ $app = require_once __DIR__.'/../bootstrap/app.php';
 
 $kernel = $app->make(Kernel::class);
 
-dd(Request::capture());
-
 $response = $kernel->handle(
     $request = Request::capture()
 )->send();
+
 $kernel->terminate($request, $response);
